75995a2b2c224c2039c684fc99c2cf90ea577fd3
Oliver Bazely <oliverbazely@gmail.com>
2025-09-19 11:51:53 +0100

    using java_path

--- build/first_setup.sh
+++ build/first_setup.sh
@@ -6,20 +6,43 @@ YELLOW='\e[33m'
 BLUE='\e[34m'
 RESET='\e[0m'
 
+if [ -n "$BASH_VERSION" ]; then
+    echo "This script is running in Bash, which is not supported"
+    exit 1
+elif [ -n "$ZSH_VERSION" ]; then
+    echo "This script is running in Zsh."
+else
+    echo "This script is running in an unknown shell."
+fi
+
 echo -e "${BLUE}Setting up binaries${RESET}"
 
-bin/Java/jdk-24.0.2.jdk/Contents/Home/bin/java --version
-if [ $? -eq 0 ]; then
+if [ -x "$(ls -d bin/Java/jdk*)/Contents/Home/bin/java" ]; then
     echo -e "${GREEN}Java already available${RESET}"
 else
     echo -e "${RED}Java not found. Downloading...${RESET}"
     rm -rf bin/Java
     mkdir -p bin/Java/
-    curl -L "https://download.oracle.com/java/24/latest/jdk-24_macos-aarch64_bin.tar.gz" | tar -xzf - --strip-components=1 -C bin/Java/
-    bin/Java/jdk-24.0.2.jdk/Contents/Home/bin/java --version
+    arch=$(uname -m)
+    # echo $arch
+
+    if [ "$arch" = "x86_64" ]; then
+        echo "Intel CPU"
+        curl -L "https://download.oracle.com/java/25/latest/jdk-25_macos-x64_bin.tar.gz" -o "bin/Java/jdk.tar.gz"
+    else
+        echo "Apple Silicon"
+        curl -L "https://download.oracle.com/java/25/latest/jdk-25_macos-aarch64_bin.tar.gz" -o "bin/Java/jdk.tar.gz"
+    fi
+
+    tar -xzf bin/Java/jdk.tar.gz --strip-components=1 -C bin/Java/
+    rm bin/Java/jdk.tar.gz
 fi
 
-bin/Java/jdk-24.0.2.jdk/Contents/Home/bin/java -jar bin/epubcheck-5.2.1/epubcheck.jar --version
+java_path="$(ls -d bin/Java/jdk*)/Contents/Home/bin/java"
+echo "Using Java at path: $java_path"
+"$java_path" --version
+
+$java_path -jar bin/epubcheck-5.2.1/epubcheck.jar --version
 if [ $? -eq 0 ]; then
     echo -e "${GREEN}epubchecker already available${RESET}"
     echo -e "${GREEN}Setup complete${RESET}"
@@ -30,13 +53,7 @@ else
     curl -L "https://github.com/w3c/epubcheck/releases/download/v5.2.1/epubcheck-5.2.1.zip" -o "epub.zip"
     unzip "epub.zip" -d bin
     rm epub.zip
-fi
-
-bin/Java/jdk-24.0.2.jdk/Contents/Home/bin/java -jar bin/epubcheck-5.2.1/epubcheck.jar --version
-if [ $? -eq 0 ]; then
-    echo -e "${GREEN}Setup complete${RESET}"
-else
-    echo -e "${RED}Setup failed!{RESET}"
+    bin/Java/jdk-24.0.2.jdk/Contents/Home/bin/java -jar bin/epubcheck-5.2.1/epubcheck.jar --version
 fi
 
 
